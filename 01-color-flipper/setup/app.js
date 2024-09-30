53ca5d8c75cf6547415f8a9a942ab48069f8ac82
Alan A Cruce <alex.cruce@gmail.com>
2024-09-30 08:18:57 -0500

    Project 1 build 1

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,24 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// variables
+const body = document.querySelector("body");
+const btn = document.querySelector(".btn");
+const color = document.querySelector(".color");
+
+// functions
+const newColor = () => {
+    const randNum = Math.trunc(Math.random() * 4);
+    return colors[randNum];
+}
+
+const changeColor = (nextColor) => {
+    body.style.backgroundColor = nextColor;
+}
+
+
+// event listener
+btn.addEventListener('click', () => {
+    const nextColor = newColor();
+    changeColor(nextColor);
+
+})
